14775936857d46f48ade42f748ac0ed549c94fec
Geovane Freitas <geovane.freitasc@gmail.com>
2021-06-07 11:29:54 -0300

    Alterando e destruindo buckets na aws

--- 01-terraform-basico/main.tf
+++ 01-terraform-basico/main.tf
@@ -5,8 +5,8 @@ terraform {
 
   required_providers {
     aws = {
-//      indica que é um provider oficial
-      source = "hashicorp/aws"
+      //      indica que é um provider oficial
+      source  = "hashicorp/aws"
       version = "3.44.0"
     }
   }
@@ -15,22 +15,33 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-//  OBS: se eu não tivesse feito a configuração: aws configure via cli, onde eu preenchi o meu
-//  Access Key ID e Secret Access Key  via linha de comando, eu teria que colocar esses valores aqui
-//  o que seria altamente inseguro ou então utilizando variáveis de ambiente
+  //  OBS: se eu não tivesse feito a configuração: aws configure via cli, onde eu preenchi o meu
+  //  Access Key ID e Secret Access Key  via linha de comando, eu teria que colocar esses valores aqui
+  //  o que seria altamente inseguro ou então utilizando variáveis de ambiente
 }
 
 // criação de recurso: recurso do tipo aws_s3_bucket com o nome my-test-bucket pra identificar ele
 
 resource "aws_s3_bucket" "my-test-bucket" {
-//
-  bucket = "my-tf-test-bucket-839839288392839"
-  acl = "private"
+  //
+  bucket = "my-tf-test-bucket-8398392883232323"
+  acl    = "private"
 
   tags = {
-    Name = "My bucket"
+    Name        = "My bucket"
     Environment = "Dev"
-    Managedby = "Terraform"
+    ManagedBy   = "Terraform"
+    Owner       = "Geovane Freitas Camara"
+    UpdatedAt   = "2021-06-08"
   }
 
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
--- README.md
+++ README.md
@@ -1,12 +1,48 @@
-###Alguns comandos do terraform 
-####Download do provider e módulos definidos
+### Alguns comandos do terraform 
+#### Download do provider e módulos definidos
     terraform init
 
-#### Gerar um um plano de execução
+#### Gerar um plano de execução
 Gera um plano de execução especulativo, mostrando quais ações o Terraform
 irá aplicar a configuração atual. Este comando não vai realmente executar as ações planejadas.
 
 Você pode opcionalmente salvar o plano em um arquivo, que você pode então passar para
 o comando "apply" para executar exatamente as ações descritas no plano.
 
+    terraform plan
+
+#### Gerar um plano de execução
+Cria ou atualiza a infraestrutura de acordo com a configuração do Terraform
+    
     terraform apply
+
+#### Validação 
+Valida os arquivos de configuração para saber se nossas configurações estão de acordo 
+com as especificações. 
+
+    terraform validate
+
+#### Formatação
+Deixa o código no padrão que o terraform recomenda
+
+    terraform fmt
+
+#### Alteração
+Quando eu passo a flag -out, estou dizendo para o terraform escrever no arquivo "tfplan.out"
+com o output do comando terraform plan e em seguida peço para aplicar as atualizações
+
+    terraform plan -out="tfplan.out"
+
+    terraform apply "tfplan.out"
+
+#### Destruir
+    
+    terraform destroy
+
+
+### EC2 AWS
+
+#### Instancias 
+
+É uma máquina virtual, que pode ser linux ou windoes, onde você pode subir e instalar nela o que você quiser como servidores, 
+banco de dados, tudo que é possível instalar em um servidor normal.  
